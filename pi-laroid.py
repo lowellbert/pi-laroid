3ee2e0e5ca0053d8ddc6e69a7af1b0b871f0a413
Lowell Smith <lowellbert@gmail.com>
2021-08-16 09:52:28 -0600

    converted script to python

--- pi-laroid.py
+++ pi-laroid.py
@@ -3,6 +3,8 @@ from picamera import PiCameara
 from time import sleep
 
 camera = PiCamera()
+camera.resolution = (3280, 2464) #Max resolution = 3280 x 2464 #print resolution = 512, 384
+
 
 def take_picture_and_print():
     #"raspistill command details
@@ -12,10 +14,11 @@ def take_picture_and_print():
     # -h = height: set image height
     # -o = output:  Output filename (to write to stdout, use '-o -'). If not specified, no file is saved
     # -lp = print a file "
-    raspistill -n -t 200 -w 512 -h -384 -o - | lp 
+    #raspistill -n -t 200 -w 512 -h -384 -o - | lp 
 
 def main():
     take_picture_and_print()
+    camera.start_preview()
 
 
 main()
